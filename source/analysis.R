a0d4404b5920ce150b61bce2399644fd10d8aa39
kaymtoun <kaymtoun@uw.edu>
2022-02-24 19:40:04 -0800

    New Updates 2/24/2022-KT

--- source/analysis.R
+++ source/analysis.R
@@ -2,14 +2,11 @@
 #INFO 201 AH
 #Data Visualization: Incarceration 
 
-
 # Loading Packages 
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(maps)
-#library(mapproj)
-#library(patchwork)
 library(RColorBrewer)  
 display.brewer.all()
 
@@ -21,6 +18,7 @@ incarceration_data <- read.csv("https://raw.githubusercontent.com/vera-institute
 #Part 1: Introduction/summary information
 #Questions: What is the comparison between female and male incarceration from 
 #the data set.
+
 #num of rows/observations in Incarceration data 
 obs_incarceration <- nrow(incarceration_data)
 print(obs_incarceration)
@@ -38,38 +36,51 @@ range_years <- range(incarceration_data$year)
 
 #1970-2018 
 
-#What is the total number of females incarcerated in 2018?
-num_female_2018 <- incarceration_data%>%
-  #drop_na() %>%
-  filter(year == 2018) %>%
-  group_by(female_jail_pop)%>%
-  summarise(female_jail_pop = sum(female_jail_pop)) %>% 
-  nrow()
-
-#What is the total number of females incarcerated in 1970?
-num_female_1970 <- incarceration_data%>%
-  #drop_na() %>%
-  filter(year == 1970) %>%
-  group_by(female_jail_pop)%>%
-  summarise(female_jail_pop = sum(female_jail_pop)) %>% 
-  nrow()
-
-#What is the total number of males incarcerated in 2018?
-num_male_2018 <- incarceration_data %>%
-  #drop_na() %>%
-  filter(year == 2018) %>%
-  group_by(male_jail_pop)%>%
-  summarise(male_jail_pop = sum(male_jail_pop)) %>% 
-  nrow()
-
-#Testing, I am trying to find the total population of incarcerated 
-# In the year 2018.
-black_male_2018 <- incarceration_data %>%
- # drop_na() %>%
+#What is the total number of black females incarcerated in 2000?
+black_female_2000 <- incarceration_data%>%
   group_by(year)%>%
-  summarise(black_jail_pop = sum(black_jail_pop)) %>% 
-  filter(year == 1990) %>%
-  pull(black_jail_pop)
+  summarise(black_female_prison_pop = sum(black_female_prison_pop, na.rm = T))%>%
+  filter(year == "2000")%>%
+  arrange(-year) %>%
+  pull(black_female_prison_pop)
+
+# 17407
+
+#What is the total number of black females incarcerated in 1970?
+black_female_1970 <- incarceration_data%>%
+  group_by(year)%>%
+  summarise(black_female_prison_pop = sum(black_female_prison_pop, na.rm = T))%>%
+  filter(year == "1970")%>%
+  arrange(-year) %>%
+  pull(black_female_prison_pop)
+
+#86
+
+#What is the total number of white females incarcerated in 2000?
+white_female_2000 <- incarceration_data%>%
+  group_by(year)%>%
+  summarise(white_female_prison_pop= sum(white_female_prison_pop, na.rm = T))%>%
+  filter(year == "2000")%>%
+  arrange(-year) %>%
+  pull(white_female_prison_pop)
+
+# 18010
+  
+# State with Highest percentage of black female incarcerated in the U.S.
+highest_percent_femaleB <- incarceration_data %>%
+  drop_na() %>%
+  filter(black_female_prison_pop == max(black_female_prison_pop))%>%
+  pull(state)
+
+# NY
+
+# State with Highest percentage of white female incarcerated in the U.S.
+highest_percent_femaleW <- incarceration_data %>%
+  drop_na() %>%
+  filter(white_female_prison_pop == max(white_female_prison_pop))%>%
+  pull(state)
+
+# CA
 
 #State with most female jail population
  state_female_incarceration <- incarceration_data %>%
@@ -78,41 +89,33 @@ black_male_2018 <- incarceration_data %>%
     pull(state)
   print(state_female_incarceration)
 
-#State with most male jail population
-  state_male_incarceration <- incarceration_data %>%
-    drop_na() %>%
-    filter(male_jail_pop == max(male_jail_pop)) %>%
-    pull(state)
-  print(state_male_incarceration)
-  
-  
-#Part 2 #############################################################
+#NY
+
+#Part 2 ############################################################
 # Trends over time 
-# Number of females jailed over the years
-# NO COLOR!-KAYLA
+# Number of white females incarcerated over the years 1970-2010
   chart_1 <- ggplot(data = incarceration_data) +
-    geom_col(mapping = aes(x = year , y = female_jail_pop)) +
-    labs(title = "Female Jail Population Over time")  +
-    scale_fill_brewer(palette="Blues")
-  
+    geom_col(mapping = aes(x = year , y = white_female_prison_pop, fill = year)) +
+    scale_fill_distiller(palette = "PuBu")+
+    labs(title = "White Female Jail Population Over Time") +
+    scale_alpha_continuous(10, 20)
+
   
 #Part 3 ############################################################
 # Compares two variables 
-# Number of male incarceration over the years
-# NO COLOR!-KAYLA
-chart_2 <- ggplot(data = incarceration_data) +
-    geom_col(mapping = aes(x = year, y = male_jail_pop)) +
-    labs(title = "Male Jail Population Over Time") +
-    scale_fill_brewer(palette = "Blues")
-    
+# Number of black female incarceration over the years 1970-2010
+  chart_2 <- ggplot(data = incarceration_data) +
+    geom_col(mapping = aes(x = year , y = black_female_prison_pop, fill = year)) +
+    scale_fill_distiller(palette = "PuRd")+  ## feel free to change the palette as you find best
+    labs(title = "Black Female Jail Population Over time")  +
+    scale_alpha_continuous(10, 20)    
 
 #Part 4 ############################################################
 # Map
+county_mod <- incarceration_data %>%
+    group_by(county_name) %>%
+    select(year, fips, black_female_prison_pop, county_name, state)
 
-counties_mod <- incarceration_data %>%
-    group_by(female_jail_pop)%>%
-   filter(year == 2018)%>%
-  summarise(female_jail_pop = sum(female_jail_pop))
     
 #Map showing incarceration of females in Washington.
 #Compare the regions of incarcerated females in Washington
@@ -121,30 +124,35 @@ county_shapes <- map_data("county") %>%
   unite(polyname, region, subregion, sep = ",") %>%
   left_join(county.fips, by="polyname")
 
-map_data <- county_shapes %>%
-  left_join(counties_mod, by ="fips") %>%
-  filter(state == "washington" & county != "unknown")
+map_data <- left_join(county_mod, county_shapes)
+
+ny_data <- map_data %>%
+  filter(state == "NY", na.rm = TRUE) %>%
+  drop_na()
 
 #Blank Theme
   blank_theme <- theme_bw() +
     theme(
-      axis.line = element_blank(), # remove axis lines 
-      axis.text = element_blank(), #remove axis labels 
-      axis.ticks = element_blank(), #remove axis ticks
-      axis.title = element_blank(), #remove axis tiles 
-      plot.background = element_blank(), #remove gray background
-      panel.grid.major = element_blank(), #remove major grid lines 
-      panel.grid.minor = element_blank(), #remove minor grid lines
-      panel.border = element_blank() # remove border around plot
+      axis.line = element_blank(), 
+      axis.text = element_blank(), 
+      axis.ticks = element_blank(), 
+      axis.title = element_blank(), 
+      plot.background = element_blank(), 
+      panel.grid.major = element_blank(),  
+      panel.grid.minor = element_blank(), 
+      panel.border = element_blank()  
     )
   
 #Making Map
-  incarceration_map <-ggplot(map_data) +
+  incarceration_map <-ggplot(ny_data) +
     geom_polygon(
-      mapping = aes(x = long, y = lat, group = group),
+      mapping = aes(x = long, y = lat, group = group, fill = black_female_prison_pop),
       color = "grey", size = 0.3
     ) +
   coord_map() +
-    scale_fill_continuous(limits = c(0, map_data$total_jail_pop),na.value = "white", low ="yellow", high ="red") +
+    scale_fill_continuous(limits = c(0, max(ny_data$black_female_prison_pop)),na.value = "white", low ="blue", high ="red") +
     blank_theme +
-    ggtitle("Female Incarceration in the United States")
+    ggtitle("Black Female Incarceration in New York State") 
+    
+  
+    
